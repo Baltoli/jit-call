29620b3aea10ddf373c7cfd40af9e1c3751bc5a3
Bruce Collie <brucecollie82@gmail.com>
2020-04-10 12:20:13 +0100

    Test BC loading

--- test/load_module.cpp
+++ test/load_module.cpp
@@ -4,9 +4,19 @@
 
 #include <iostream>
 
+#define INPUT(fn) INPUT_DIR #fn
+
 using namespace jitcall::test;
 
 TEST_CASE("Can load IR files into test cases") {
-  auto mod = loadModule(INPUT_DIR "/empty.ll");
-  REQUIRE(mod);
+  SECTION("Empty IR file") {
+    auto mod = loadModule(INPUT(empty.ll));
+    REQUIRE(mod);
+  }
+
+  SECTION("Non-empty bitcode file") {
+    auto mod = loadModule(INPUT(yneg.bc));
+    REQUIRE(mod);
+    REQUIRE(mod->getFunction("func"));
+  }
 }
