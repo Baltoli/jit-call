0c4c1bcf1aff02ce19a408817aa28cf286e38163
Bruce Collie <brucecollie82@gmail.com>
2020-04-10 14:23:43 +0100

    Fix up loading of test files

--- test/src/load_module.cpp
+++ test/src/load_module.cpp
@@ -4,18 +4,18 @@
 
 #include <iostream>
 
-#define INPUT(fn) INPUT_DIR #fn
+#define INPUT(fn) INPUT_DIR #fn ".bc"
 
 using namespace jitcall::test;
 
 TEST_CASE("Can load IR files into test cases") {
   SECTION("Empty IR file") {
-    auto mod = loadModule(INPUT(empty.ll));
+    auto mod = loadModule(INPUT(empty));
     REQUIRE(mod);
   }
 
   SECTION("Non-empty bitcode file") {
-    auto mod = loadModule(INPUT(yneg.bc));
+    auto mod = loadModule(INPUT(yneg));
     REQUIRE(mod);
     REQUIRE(mod->getFunction("func"));
   }
