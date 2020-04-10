26f0efd791f26b311e8524eea09fe06ecfa6f766
Bruce Collie <brucecollie82@gmail.com>
2020-04-10 12:08:42 +0100

    Implement basic module loader

--- test/util.cpp
+++ test/util.cpp
@@ -1,10 +1,21 @@
 #include "util.h"
 
+#include <jitcall/context.h>
+
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/MemoryBuffer.h>
+#include <llvm/Support/SourceMgr.h>
+
+using namespace llvm;
+
 namespace jitcall {
 namespace test {
 
-std::unique_ptr<llvm::Module> LoadModule(std::string const &path) {
-  return nullptr;
+std::unique_ptr<Module> LoadModule(std::string const &path) {
+  auto &ctx = ThreadContext::get();
+  auto err = SMDiagnostic{};
+
+  return parseIRFile(path, err, ctx);
 }
 
 } // namespace test
