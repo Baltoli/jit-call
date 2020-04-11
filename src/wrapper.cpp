160c7fb6706be061c4577539c5b1792a85bfb57f
Bruce Collie <brucecollie82@gmail.com>
2020-04-11 10:02:09 +0100

    Reformat

--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -4,13 +4,20 @@ using namespace llvm;
 
 namespace jitcall {
 
-Wrapper::Wrapper(Function *impl) : Impl(impl) {}
+Wrapper::Wrapper(Function *impl) : Impl(impl) {
+}
 
-Module *Wrapper::module() const { return Impl->getParent(); }
+Module *Wrapper::module() const {
+  return Impl->getParent();
+}
 
-DataLayout Wrapper::getDataLayout() const { return DataLayout(module()); }
+DataLayout Wrapper::getDataLayout() const {
+  return DataLayout(module());
+}
 
-LLVMContext &Wrapper::context() const { return Impl->getContext(); }
+LLVMContext &Wrapper::context() const {
+  return Impl->getContext();
+}
 
 TypeSize Wrapper::getTypeSize(llvm::Type *ty) const {
   return getDataLayout().getTypeStoreSize(ty);
